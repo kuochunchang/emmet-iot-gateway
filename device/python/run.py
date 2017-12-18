376731fb57b04b0ee94764c42da18c0e71780481
Kuo-Chun Chang <guojun.tw@gmail.com>
2017-12-18 18:12:35 +0800

    ...

---
+++ device/python/run.py
@@ -0,0 +1,23 @@
+import time
+
+INPUT_GOIO = [10]
+OUTPUT_GOIO = [20]
+
+current_data = {}
+
+
+# initial current data
+for i in INPUT_GOIO:
+    current_data[i] = 100
+    print(i, ": ", current_data[i])
+
+for o in OUTPUT_GOIO:
+    current_data[o] = 200
+    print(o, ": ", current_data[o])
+
+
+while True:
+    time.sleep(1)
+
+    for key, value in current_data.items():
+        print("%s: %s" % (key, value))
